88db1967f4da1dba45e4b3d0340a54b49c9cbeb1
Popqwas <sawqpop@gmail.com>
2022-11-12 11:49:01 -0500

    Created getSpeed method

--- src/main/java/frc/robot/extensions/SwerveModule.java
+++ src/main/java/frc/robot/extensions/SwerveModule.java
@@ -5,14 +5,14 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedController;
 import frc.robot.Constants;
 
 public class SwerveModule {
     WPI_TalonFX driveMotor;
     WPI_TalonFX turnMotor;
-    
 
-    public SwerveModule( WPI_TalonFX dMotor, WPI_TalonFX tMotor ) {
+    public SwerveModule(WPI_TalonFX dMotor, WPI_TalonFX tMotor) {
 
         driveMotor = dMotor;
         turnMotor = tMotor;
@@ -21,62 +21,93 @@ public class SwerveModule {
 
     /**
      * This methods gives back turn encoder position.
+     * 
      * @param selectedEncoder
      * @return value of selected encoder
      */
-    public double getDriveEncoderTicks( ){
+    public double getDriveEncoderTicks() {
         return driveMotor.getSelectedSensorPosition();
     }
 
     /**
-     * This methods gives back drive encoder position.
+     * This methods returns turn encoder velocity in ticks per 100ms.
+     * 
      * @param selectedEncoder
      * @return value of selected encoder
      */
-    public double getTurnEncoderTicks( ){
-      return turnMotor.getSelectedSensorPosition();
+    public double getDriveEncoderVelocityTicks() {
+        return driveMotor.getSelectedSensorVelocity();
     }
+
+    /**
+     * This methods gives back drive encoder position.
+     * 
+     * @param selectedEncoder
+     * @return value of selected encoder
+     */
+    public double getTurnEncoderTicks() {
+        return turnMotor.getSelectedSensorPosition();
+    }
+
     /**
      * Calculates angles
+     * 
      * @return angle
      */
-    public double getAngle(){
-        return ticksToAngle(getTurnEncoderTicks(),Constants.turnMotorGearRatio);
-    };
-    /**
-     *  Calculate distance
-     * @return distance
-     */
-    public double getDistance(){
-        return ticksToPosition(getDriveEncoderTicks( ), Constants.wheelDiameter , Constants.driveMotorGearRatio);
+    public double getAngle() {
+        return ticksToAngle(getTurnEncoderTicks(), Constants.turnMotorGearRatio);
     };
 
-    //TO DO SATURDAY;
-    public double getSpeed(){
-        return 10;
+    /**
+     * Calculate distance
+     * 
+     * @return distance
+     */
+    public double getDistance() {
+        return ticksToPosition(getDriveEncoderTicks(), Constants.wheelDiameter, Constants.driveMotorGearRatio);
+    };
+
+    // TO DO SATURDAY;
+    // In theory, should return inches per second
+    public double getSpeed(double ticksVelocity, double wheelDiameter) {
+        double radius = wheelDiameter / 2;
+        double turnsPerSec = ticksVelocity * 1000;
+        double speedPerSec = turnsPerSec * ((2 * Math.PI * radius) / Constants.encoderTicksPerTurn);
+
+        // In theory, should return inches per 100ms
+        /*
+         * double radius = wheelDiameter / 2;
+         * double turnsPer100MS = driveMotor.getSelectedSensorVelocity();
+         * double speed = turnsPer100MS * ((2 * Math.PI * radius) /
+         * Constants.encoderTicksPerTurn);
+         */
+        return speedPerSec;
     };
 
     // Takes the rotation or internal ticks of Falcon Encoder and turn them to a
-    // travel distance. gear ratio A:1 means, 1/A. //Assuming all motors have same encoder per ticks.
+    // travel distance. gear ratio A:1 means, 1/A. //Assuming all motors have same
+    // encoder per ticks.
     public double ticksToAngle(double ticks, double gearRatio) {
-      double nbTurnMotor = ticks / Constants.encoderTicksPerTurn;
-      double nbTurnWheel = nbTurnMotor * gearRatio;
-      double angleOfWheel = nbTurnWheel * 360;
-      return angleOfWheel;
+        double nbTurnMotor = ticks / Constants.encoderTicksPerTurn;
+        double nbTurnWheel = nbTurnMotor * gearRatio;
+        double angleOfWheel = nbTurnWheel * 360;
+        return angleOfWheel;
 
     }
 
     // Takes the rotation or internal ticks of Falcon Encoder and turn them to a
     // travel distance. gear ratio A:1 means, 1/A.
     public double ticksToPosition(double ticks, double wheelDiameter, double gearRatio) {
-      double nbTurnMotor = ticks / Constants.encoderTicksPerTurn;
-      double nbTurnWheel = nbTurnMotor * gearRatio;
-      double distanceTravel = nbTurnWheel * Math.PI * wheelDiameter;
-      return distanceTravel;
+        double nbTurnMotor = ticks / Constants.encoderTicksPerTurn;
+        double nbTurnWheel = nbTurnMotor * gearRatio;
+        double distanceTravel = nbTurnWheel * Math.PI * wheelDiameter;
+        return distanceTravel;
     }
 
-    // Gives back the distance of the drive motor and the angle of the turn motor, as a swerve Module state Object 
-    public SwerveModuleState getModuleState(){
-        return new SwerveModuleState(getSpeed(),new Rotation2d(getAngle()/(2*3.1416)));
+    // Gives back the distance of the drive motor and the angle of the turn motor,
+    // as a swerve Module state Object
+    public SwerveModuleState getModuleState() {
+        return new SwerveModuleState(getSpeed(getDriveEncoderVelocityTicks(), Constants.wheelDiameter),
+                new Rotation2d(getAngle() / (2 * 3.1416)));
     };
 }
